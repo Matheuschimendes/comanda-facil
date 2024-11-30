d3ba4bde6e4ff4ec9bab3e46cf833b8baca9d238
Curso de JS <matheuschiimenndes@gmail.com>
2024-11-30 18:13:43 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-const Home = () =>{
+import { Button } from "./_components/ui/button";
+
+const Home = () => {
   return (
-      <div>Ola </div>
-  )
-}
+    <div>
+      <h1 className="text-red-500">Home</h1>
+      <Button>Botao</Button>
+    </div>
+  );
+};
 
 export default Home;
