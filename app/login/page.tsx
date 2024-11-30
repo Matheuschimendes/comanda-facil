e5b431d011b1df67f0d31779a7234e7f33db0319
Curso de JS <matheuschiimenndes@gmail.com>
2024-11-30 17:40:31 -0300

    novo

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+const PageLogin = () =>{
+    return (
+        <div>Ola</div>
+    )
+}
+
+export default PageLogin;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+const Home = () =>{
+  return (
+      <div>Ola</div>
+  )
+}
+
+export default Home;
